d333ceeecdd85eba9d82eddb8fe8332daeff0388
NuRifat <najimullahrifat@gmail.com>
2025-12-11 16:12:18 +0600

    Updating urls name

--- event_management_project/urls.py
+++ event_management_project/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from events.views import home
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('events/', include('events.urls')),
-    path('', home, name='home'),  # root redirects to /events/
+    path('events/', include('events.urls')),  # root redirects to /events/
 ] + debug_toolbar_urls()
 
 
--- events/urls.py
+++ events/urls.py
@@ -6,12 +6,12 @@ from events.views import (
 
 urlpatterns = [
     path('home/', organizer_dashboard, name="home-page"),  # /events/home/
-    path('', events_page, name="events-page"),             # /events/
-    path("<int:id>/", event_details, name="event-details"),         # /events/<id>/
-    path("<int:id>/edit/", event_edit, name="event-edit"),          # /events/<id>/edit/
+    path('events/', events_page, name="events-page"),             # /events/
+    path("events/<int:id>/", event_details, name="event-details"),         # /events/<id>/
+    path("events/<int:id>/edit/", event_edit, name="event-edit"),          # /events/<id>/edit/
     path('event_form/', create_event, name="create-event"),         # /events/event_form/
     path('participants/', participants_page, name="participants-page"),  # /events/participants/
-    path("<int:id>/participants/", participant_details, name="participant-details"),  # optional adjust if needed
+    path("participants/<int:id>/", participant_details, name="participant-details"),  # optional adjust if needed
     path("participant_form/", create_participant, name="create-participant"),
     path("delete_participate/<int:id>/", participant_delete, name="delete-participant"),
     path("categories/", categories, name="categories-page"),
